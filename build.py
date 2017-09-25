f87ec666c71cd45f0e7ed1ef7844e7da7245d712
Josef Baltz <josef@baltz.net>
2017-09-24 22:43:34 -0500

    File Cleanup

--- build.py
+++ build.py
@@ -1,19 +1,12 @@
 from cx_Freeze import setup, Executable
-
 base = None
-
-
 executables = [Executable("shrok.py", base=base)]
-
 packages = ["discord", "discord.ext", "json", "urllib"]
 options = {
     'build_exe': {
-
         'packages':packages,
     },
-
 }
-
 setup(
     name = "Shrok",
     options = options,
